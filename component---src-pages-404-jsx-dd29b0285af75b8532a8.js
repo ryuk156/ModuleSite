cd5d924c8af24ad8645f2cc888061b52671e1fee
ryuk156 <yp15601560@gmail.com>
2021-05-26 13:44:05 +0530

    build

---
+++ component---src-pages-404-jsx-dd29b0285af75b8532a8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{pssB:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("TJpk"),r=t.n(o),c=t("hpys"),i=t("IpnI"),s=t.n(i);n.default=function(){return l.a.createElement(c.a,null,l.a.createElement("div",{className:"about-container"},l.a.createElement(r.a,{title:"Not Found | "+s.a.siteTitle})),l.a.createElement("center",null,l.a.createElement("h1",null,"404"),l.a.createElement("h4",null,"This is not the page you are looking for...")))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-dd29b0285af75b8532a8.js.map
