9642f5bfb2f1e0b80fc6b5ad46ab6d514e9afab7
ryuk156 <yp15601560@gmail.com>
2021-07-08 11:37:27 +0530

    fix: jenkins

--- module-generation/scrape.py
+++ module-generation/scrape.py
@@ -59,9 +59,9 @@ for module in range(len(modules)):
             imageFile.close()
         else:
             print("No banner found on "+ moduleName+",resolving with default banner")
-            sourceImage=open("defaultBanner.png", "rb+")
-            readSourceImage=sourceImage.read()
-            defaultImageFile = open(moduleDirSrc+"/cover.png", "wb+")
-            defaultImageFile.write(readSourceImage)
-            defaultImageFile.close()
+            # sourceImage=open("./module-generation/defaultBanner.png", "rb+")
+            # readSourceImage=sourceImage.read()
+            # defaultImageFile = open(moduleDirSrc+"/cover.png", "wb+")
+            # defaultImageFile.write(readSourceImage)
+            # defaultImageFile.close()
             
